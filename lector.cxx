55cafcc2e3aed9542b86a4591f5933e19a76f268
davillen <120469951+davillen@users.noreply.github.com>
2023-01-02 13:27:46 +0100

    Initial commit

---
+++ lector.cxx
@@ -0,0 +1,50 @@
+/*
+ * lector.cxx
+ * 
+ * Author: David Villen - david@villen.cat
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+FILE *Fitxer;
+char i;
+
+int main(void)
+{
+	char NomFit[30];
+	printf("Arxiu de texte per obrir: ");
+	scanf("%s",NomFit);
+	 if ((Fitxer=fopen(NomFit,"r"))==NULL)
+	 {
+		 printf("Impossible obrir el fitxer");
+		 exit(0);
+		 }
+	do
+	{ 
+		i=fgetc(Fitxer);
+		printf("%c",i);
+	}while (i!=EOF);
+	fclose(Fitxer); 
+	return 0;
+}
+
